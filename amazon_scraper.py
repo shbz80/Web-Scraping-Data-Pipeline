f2add68f04f7d42587ecce6b019a8209c9256104
Shahbaz Khader <shahbazkhader@gmail.com>
2022-01-27 21:14:20 +0100

    Implements a method to check if the current page is the last one.

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -1,6 +1,6 @@
 import time
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 class AmazonScraper():
     def __init__(self, url) -> None:
@@ -13,27 +13,53 @@ class AmazonScraper():
         pass
 
     def connect_to_link(self):
-        # inits selenium and gets the lik to the category
+        """
+        Initializes Selenium and connects to the starting url
+        """
+        # inits selenium and gets to the link
         self.driver = webdriver.Chrome()
         self.driver.get(self.url)
-        # time.sleep(20)
 
     def sort_by_reviews(self):
+        """
+        Sort the results by number of reviews
+        """
+        # click on the sort by dropdown button
         xpath = '//span[@class = "a-dropdown-container"]'
         sort_dropdown = self.driver.find_element_by_xpath(xpath)
         xpath = './/span[@class="a-button-text a-declarative"]'
         sort_dropdown_button = sort_dropdown.find_element_by_xpath(xpath)
         sort_dropdown_button.click()
+
+        # click the sort criterion: sort by reviews
         xpath = '//div[@class="a-popover-inner"]'
         temp_tag = self.driver.find_element_by_xpath(xpath)
         sort_criteria = temp_tag.find_elements_by_xpath('./ul/li')
         xpath = './a'
         sort_criteria[-1].find_element_by_xpath(xpath).click()
+        time.sleep(1)
+
+    def is_last_page(self):
+        """
+        Checks if the current reult page is the last one
+        """
+        xpath = '//span[@class="s-pagination-strip"]'
+        pagination_strip = self.driver.find_element_by_xpath(xpath)
+        elements = pagination_strip.find_elements_by_xpath('./*')
+        last_element = elements[-1]
+        if last_element.find_elements(By.ID, "aria-disabled"):
+            return True
+        else:
+            return False
+
+
 
 if __name__ == '__main__':
     url = 'https://www.amazon.com/s?i=stripbooks&rh=n%3A25&fs=true&qid=1643228276&ref=sr_pg_1'
     amazonScraper = AmazonScraper(url)
     amazonScraper.connect_to_link()
     amazonScraper.sort_by_reviews()
+    is_last_page = amazonScraper.is_last_page()
+    
     while True:
         pass
