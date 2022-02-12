de844f00131a539941790cec01bcb5c3f9d59d9b
Shahbaz Khader <shahbazkhader@gmail.com>
2022-02-12 20:08:40 +0100

    docs: addes doc strings to the utils module

--- utils.py
+++ utils.py
@@ -1,10 +1,20 @@
-
+"""This module implements some useful util functions and classes
+"""
 import os
 
-def create_dir_if_not_exists(dir):
+def create_dir_if_not_exists(dir: str) -> bool:
+    """Creates a directory at the given path if it doesn't exist
+
+    Args:
+        dir (str): path for directory       
+
+    Returns:
+        bool: if the directory was created (True) or if it already 
+        existed (False)
+    """
     try:
         os.mkdir(dir)
         return True
     except FileExistsError:
-        print(f'The directory "{dir}"" exists')
+        print(f'The directory "{dir}" exists')
         return False
