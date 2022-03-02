6559ff36ce074b65666132936b16b55c219ee9fd
Shahbaz Khader <shahbazkhader@gmail.com>
2022-03-02 15:20:14 +0100

    feat: adds some util functions

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 """This module implements some useful util functions and classes
 """
 import os
-
+from os import listdir, walk
+from os.path import isfile, join
 # some constants
 PAGE_SLEEP_TIME = 1
 
@@ -22,3 +23,23 @@ def create_dir_if_not_exists(dir: str) -> bool:
     except FileExistsError:
         print(f'The directory "{dir}" exists')
         return False
+
+def get_list_of_files(dir: str) -> list[str]:
+    """Get a list of file in the given dir"""
+    if not os.path.isdir(dir):
+        raise Exception('Not a vald directory.')
+
+    return [f for f in listdir(dir)
+                     if isfile(join(dir, f))]
+
+def get_list_of_dirs(dir: str) -> list[str]:
+    """Get a list of dirs in the given dir"""
+    return next(walk(dir))[1]
+
+def is_dir_present(dir, path):
+    """Does the given dir exis in the given path(dir)"""
+    return dir in get_list_of_dirs(path)
+
+def is_file_present(file, path):
+    """Does the given file exis in the given path(dir)"""
+    return file in get_list_of_files(path)
